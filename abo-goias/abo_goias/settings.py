98f40260622e3c9aead6125e82059092fded607e
Tecnologia <tecnologia@upestrategia.com.br>
2026-06-26 11:27:18 -0300

    feature: adicionado contexto de usuário logado e melhorias na interface

--- abo-goias/abo_goias/settings.py
+++ abo-goias/abo_goias/settings.py
@@ -199,6 +199,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "gestao_cme.context_processors.usuario_logado",
             ],
         },
     },
